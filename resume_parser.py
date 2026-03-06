d63a0b5bca588cb5e9a334a67c01535a07ccd4ad
sdeepaknetha <sdeepaknetha@gmail.com>
2026-03-06 08:38:39 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,16 +1,15 @@
 def extract_text_from_resume(file):
 
     try:
-        file_content = file.read()
+        content = file.read()
 
-        # Try UTF-8 decoding
         try:
-            text = file_content.decode("utf-8")
+            text = content.decode("utf-8")
         except:
-            text = file_content.decode("latin-1")
+            text = content.decode("latin-1")
 
         return text.lower()
 
     except Exception as e:
-        print("Error reading resume:", e)
+        print("Error reading file:", e)
         return ""
