134bade5bff9da3b799e90baf296d78e74f1630f
sdeepaknetha <sdeepaknetha@gmail.com>
2026-03-06 08:17:10 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,21 +1,35 @@
 import pdfplumber
 import docx
 
+
 def extract_text_from_resume(file):
 
     filename = file.filename.lower()
 
     if filename.endswith(".pdf"):
+
+        text = ""
+
         with pdfplumber.open(file) as pdf:
-            text = ""
             for page in pdf.pages:
-                text += page.extract_text()
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text
+
         return text
 
+
     elif filename.endswith(".docx"):
+
         doc = docx.Document(file)
         text = "\n".join([p.text for p in doc.paragraphs])
         return text
 
-    else:
+
+    elif filename.endswith(".txt"):
+
         return file.read().decode("utf-8")
+
+
+    else:
+        return ""
